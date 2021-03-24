7e74356f093807de0fc8e28dc9ea7de7f9020dc6
mxmlnhrc <maxihenri03@gmail.com>
2021-03-24 19:18:27 +0100

    edited database file, from class to def view

--- Database.py
+++ Database.py
@@ -3,24 +3,26 @@ import sys
 import sqlite3
 
 
-class Database:
-    def CreateDatabase(name):
-        # Check if database exists
-        if os.path.exists(name + ".db"):
-            print("Database already exists.")
-            sys.exit(0)
+def CreateDatabase(db="contact_book"):
+    # Check if database exists
+    if os.path.exists(db + ".db"):
+        print("Database already exists.")
+        sys.exit(0)
 
-        connection = sqlite3.connect(name + ".db")
+    connection = sqlite3.connect(db + ".db")
 
-        cursor = connection.cursor()
+    cursor = connection.cursor()
 
-        sql = (
-            "CREATE TABLE personen(" "surname TEXT," "firstname TEXT," "phone INTERGER)"
-        )
+    sql = "CREATE TABLE personen(" "lastname TEXT," "firstname TEXT," "phone INTERGER)"
 
-        cursor.execute(sql)
-        connection.commit()
+    cursor.execute(sql)
+    connection.commit()
 
-        print("Sucessfully created a database")
+    print("Sucessfully created a database")
 
-    # def AddPerson(firstname, surname, phone):
+
+def AddPerson(firstname, lastname, phone, db="contact_book"):
+    if not os.path.exists(db + ".db"):
+        CreateDatabase(db)
+    else:
+        print("Test")
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from Database import Database
+
+
+Database.AddPerson(firstname="Max", lastname="Heinrich", phone=455454, db="Test")
