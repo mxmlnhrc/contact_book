5d9d7a9ea9e0d8ce6b80b850fc1d236b134f849e
mxmlnhrc <maxihenri03@gmail.com>
2021-03-24 20:57:53 +0100

    added exception handling to the phone number

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from Database import AddPerson, CreateDatabase
+import phonenumbers
 
-AddPerson(firstname="Max", lastname="Heinrich", phone=int(455454))
+while True:
+    firstname = str(input("Firstname:\n"))
+    lastname = str(input("Lastname:\n"))
+    while True:
+        phone_number = str(input("Phone number:\n"))
+        try:
+            phone_number = phonenumbers.parse(phone_number, None)
+            if phonenumbers.is_possible_number(phone_number):
+                if phonenumbers.is_valid_number(phone_number):
+                    phone = phone_number
+                    break
+                else:
+                    print("This number is not valid.\nPlease try again.")
+            else:
+                print("This number is to short/long.\nPlease try again.")
+        except phonenumbers.phonenumberutil.NumberParseException as e:
+            print(e)
+
+    print("Data that is submitted:")
+    print(f"Firstname: {firstname} | Lastname: {lastname} | Phonenumber: ")
